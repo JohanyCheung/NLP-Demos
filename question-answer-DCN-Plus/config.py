42cffe3e7d51436199ad8073c9f520ad16939732
JohanyCheung <1979450016@qq.com>
2018-03-09 00:10:08 +0800

    some nlp-demos

---
+++ question-answer-DCN-Plus/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*-coding: utf8-*-
+
+'''
+配置文件
+
+
+'''
+
+class DefaultConfig(object):
+    # dataset
+    train_file = "./datasets/train-v1.1.json"
+    test_file = "./datasets/dev-v1.1.json"
+    context_maxlen = 600
+    
+    # 一些特殊符号
+    seq_end = '</s>'
+    seq_begin = '<s>'
+    pad = '<pad>'
+    unk = '<unk>'
+    
+    # cuda信息
+    use_cuda = True
+    device_id = 0
